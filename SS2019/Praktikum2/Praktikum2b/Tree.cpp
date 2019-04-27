d1ed1b56c44e5858435c68bc31b647b2a997242a
NicoGartmann <nico.gartmann97@gmail.com>
2019-04-27 12:32:05 +0200

    Grundgeruest für Klasse Tree

--- SS2019/Praktikum2/Praktikum2b/Tree.cpp
+++ SS2019/Praktikum2/Praktikum2b/Tree.cpp
@@ -12,8 +12,27 @@ using namespace std;
 
 ////////////////////////////////////
 // Ihr Code hier:
+Tree::Tree() {
+	this->anker = new TreeNode(); 
+}
 
+void Tree::addNode(string pName, int pAlter, double pEinkommen, int pPLZ) {
 
+}
 
+void Tree::deleteNode(int pNodePosID) {
+
+}
+
+bool Tree::searchNode(string pName) {
+
+}
+
+void Tree::printAll() {
+
+}
 //
+bool Tree::isEmpty() {
+	return !(this->anker->getRight()); 
+}
 ////////////////////////////////////
--- SS2019/Praktikum2/Praktikum2b/Tree.h
+++ SS2019/Praktikum2/Praktikum2b/Tree.h
@@ -15,19 +15,21 @@ class Tree {
 private:
 	///////////////////////////////////////
 	// Ihr Code hier:	
-
-
-
+	TreeNode* anker; 
+	int NodeIDCounter; 
 	//
 	////////////////////////////////////	
 
 public:
 	///////////////////////////////////////
 	// Ihr Code hier:	
-
-
-
+	Tree(); 
+	void addNode(string pName, int pAlter, double pEinkommen, int pPLZ); 
+	void deleteNode(int pNodePosID);
+	bool searchNode(string pName); 
+	void printAll(); 
 	//
+	bool isEmpty(); 
 	////////////////////////////////////
 	// friend-Funktionen sind für die Tests erforderlich und müssen unangetastet bleiben! 
 	friend TreeNode * get_anker(Tree& TN);
