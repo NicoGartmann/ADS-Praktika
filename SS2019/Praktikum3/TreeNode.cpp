408209b239d5e8de9db1dcf8d36312449e6580d2
NicoGartmann <nico.gartmann97@gmail.com>
2019-05-08 09:41:24 +0200

    TreeNode um red Attribut erweitert

---
+++ SS2019/Praktikum3/TreeNode.cpp
@@ -0,0 +1,97 @@
+/*************************************************
+* ADS Praktikum 2.2
+* TreeNode.cpp
+* Erweiterung um Hilfsfunktionen gestattet.
+*************************************************/
+#include "TreeNode.h"
+#include <iostream>
+#include <string>
+
+
+////////////////////////////////////
+// Ihr Code hier:
+TreeNode::TreeNode() {
+	this->left = nullptr;
+	this->right = nullptr;
+}
+
+TreeNode::TreeNode(int pNodePosID, int pNodeID, string pName, int pAlter, double pEinkommen, int pPLZ) {
+	this->NodePosID = pNodePosID;
+	this->NodeID = pNodeID;
+	this->Name = pName;
+	this->Alter = pAlter;
+	this->Einkommen = pEinkommen;
+	this->PLZ = pPLZ;
+	this->left = nullptr;
+	this->right = nullptr;
+}
+
+int TreeNode::getNodePosID() {
+	return this->NodePosID;
+}
+
+int TreeNode::getNodeID() {
+	return this->NodeID;
+}
+
+string TreeNode::getName() {
+	return this->Name;
+}
+
+void TreeNode::setName(string pName) {
+	this->Name = pName;
+}
+
+int TreeNode::getAlter() {
+	return this->Alter;
+}
+
+void TreeNode::setAlter(int pAlter) {
+	this->Alter = pAlter;
+}
+
+double TreeNode::getEinkommen() {
+	return this->Einkommen;
+}
+
+void TreeNode::setEinkommen(double pEinkommen) {
+	this->Einkommen = pEinkommen;
+}
+
+int TreeNode::getPLZ() {
+	return this->PLZ;
+}
+
+void TreeNode::setPLZ(int pPLZ) {
+	this->PLZ = pPLZ;
+}
+
+bool TreeNode::getRed() {
+	return this->red; 
+}
+
+void TreeNode::setRed(bool pRed) {
+	this->red = pRed; 
+}
+
+TreeNode* TreeNode::getLeft() {
+	return this->left;
+}
+
+TreeNode* TreeNode::getRight() {
+	return this->right;
+}
+
+void TreeNode::setLeft(TreeNode* pLeft) {
+	this->left = pLeft;
+}
+
+void TreeNode::setRight(TreeNode* pRight) {
+	this->right = pRight;
+}
+
+void TreeNode::print() {
+	cout << this->NodeID << " | " << this->Name << " | " << this->Alter << " | " << this->Einkommen << " | " << this->PLZ << " | " << this->NodePosID << " | " << endl;
+}
+//
+////////////////////////////////////
---
+++ SS2019/Praktikum3/TreeNode.h
@@ -0,0 +1,52 @@
+/*************************************************
+* ADS Praktikum 2.2
+* TreeNode.h
+* Erweiterung um Hilfsattribute und -funktionen gestattet, wenn erforderlich.
+*************************************************/
+#pragma once
+#include <string>
+
+using namespace std;
+
+class TreeNode {
+
+private:
+	///////////////////////////////////////
+	// Ihr Code hier:	
+	int NodePosID;
+	int NodeID;
+	string Name;
+	int Alter;
+	double Einkommen;
+	int PLZ;
+	bool red; 
+	TreeNode* left;
+	TreeNode* right;
+	//
+	////////////////////////////////////
+
+public:
+	///////////////////////////////////////
+	// Ihr Code hier:
+	TreeNode();
+	TreeNode(int pNodePosID, int pNodeID, string pName, int pAlter, double pEinkommen, int pPLZ);
+	int getNodePosID();
+	int getNodeID();
+	string getName();
+	void setName(string pName);
+	int getAlter();
+	void setAlter(int pAlter);
+	double getEinkommen();
+	void setEinkommen(double pEinkommen);
+	int getPLZ();
+	void setPLZ(int pPLZ);
+	bool getRed(); 
+	void setRed(bool pRed); 
+	TreeNode* getLeft();
+	TreeNode* getRight();
+	void setLeft(TreeNode* pLeft);
+	void setRight(TreeNode* pRight);
+	void print();
+	//
+	////////////////////////////////////
+};
