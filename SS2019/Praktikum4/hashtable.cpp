361f7b89ff6ec170bf6299968e7e7ff9e24547a5
NicoGartmann <nico.gartmann97@gmail.com>
2019-08-20 20:20:27 +0200

    Praktikum 4.2 Hashing fertig.

--- SS2019/Praktikum4/hashtable.cpp
+++ SS2019/Praktikum4/hashtable.cpp
@@ -8,7 +8,12 @@ HashTable::HashTable(int size) {
 	//*****************************
 	// implement constructor here *
 	//*****************************
-
+	this->size = size; 
+	this->hashTable = new vector<int>; 
+	this->hashTable->resize(size); 
+	for (int i = 0; i < size; i++) {
+		this->hashTable->at(i) = -1; 
+	}
 }
 
 HashTable::~HashTable()
@@ -25,6 +30,13 @@ int HashTable::hashValue(int item) {
 	//******************************************
 	// implement calculation of hashindex here *
 	//******************************************
+	for (int i = 0; i < this->getSize(); i++) {
+		index = (item + i * i) % this->size; 
+		if (this->hashTable->at(index) == -1) {
+			break; 
+		}
+		this->collisionCount++; 
+	}
 
 	return index;
 }
@@ -34,7 +46,21 @@ int HashTable::insert(int item) {
 	//******************************************
 	// implement insertion of new element here *
 	//******************************************
+	int index = this->hashValue(item); 
+	this->hashTable->at(index) = item; 
+	this->elements++; 
 
+	if (this->fuellStand() >= 70) {
+		cout << "Groesse vorher: " << this->getSize() << endl; 
+		if (this->rehashing()) {
+			cout << "Groesse nachher: " << this->getSize() << endl; 
+			cout << "Erfolgreich vergroessert.\n"; 
+		}
+		else {
+			cout << "Groesse nachher: " << this->getSize() << endl; 
+			cout << "Nicht erfolgreich.\n"; 
+		}
+	}
 	return 0; //dummy return
 }
 
@@ -53,4 +79,40 @@ int HashTable::getSize() {
 
 int HashTable::getElements() {
 	return this->elements;
+}
+
+int HashTable::fuellStand() {
+	double res = (double(this->getElements()) / this->getSize())*100;
+	return int(res);
+}
+
+int HashTable::setNewSize() {
+	int res = this->getSize() * 2; 
+	if (res > 997) {
+		return this->getSize(); 
+	}
+	else {
+		for (int i = 0; i < 168; i++) {
+			if (prim[i] >= res) {
+				res = prim[i];
+				break;
+			}
+		}
+		return res; 
+	}
+}
+
+bool  HashTable::rehashing() {
+	HashTable* newTable = new HashTable(this->setNewSize()); 
+
+	for (int i = 0; i < this->hashTable->size(); i++) {
+		if (this->hashTable->at(i) != -1) {
+			newTable->insert(this->hashTable->at(i));
+		}
+	}
+
+	this->hashTable = newTable->hashTable; 
+	this->collisionCount = newTable->collisionCount; 
+	this->size = newTable->size; 
+	return (this->getSize() == newTable->getSize()); 
 }
